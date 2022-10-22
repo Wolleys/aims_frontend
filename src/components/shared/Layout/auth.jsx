e2d5972566e74dc3f6bcb504acc84c6d5c3968e2
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-10-22 16:48:40 +0300

    Add: Auth layout component

---
+++ src/components/shared/Layout/auth.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Features from "../../../components/shared/Features";
+import { Box, Container, Grid, CssBaseline, Paper } from "@mui/material";
+
+function AuthLayout(props) {
+    const { form } = props;
+    return (
+        <Box component="main" sx={{ p: 0 }}>
+            <Container maxWidth="md">
+                <Grid container component="main" sx={{ height: '100vh' }}>
+                    <CssBaseline />
+                    <Grid item xs={false} md={6} sx={{ pr: 4 }}>
+                        <Features />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6} component={Paper} elevation={0} square>
+                        {form}
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
+}
+
+export default AuthLayout;
--- src/pages/public/login/index.jsx
+++ src/pages/public/login/index.jsx
@@ -1,24 +1,8 @@
-import React from "react";
 import SignIn from "./form";
-import Features from "../../../components/shared/Features";
-import { Box, Container, Grid, CssBaseline, Paper } from "@mui/material";
+import AuthLayout from "../../../components/shared/Layout/auth";
 
 function Login() {
-    return (
-        <Box component="main" sx={{ p: 0 }}>
-            <Container maxWidth="md">
-                <Grid container component="main" sx={{ height: '100vh' }}>
-                    <CssBaseline />
-                    <Grid item xs={false} md={6} sx={{ pr: 4 }}>
-                        <Features />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6} component={Paper} elevation={0} square>
-                        <SignIn />
-                    </Grid>
-                </Grid>
-            </Container>
-        </Box>
-    );
+  return <AuthLayout form={<SignIn />} />;
 }
 
 export default Login;
