614b53c56a9e7b881ff49efd1286554f435d12ce
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-10-27 11:59:29 +0300

    Update: Jobs table component

---
+++ src/pages/private/data/jobs.js
@@ -0,0 +1,26 @@
+export const jobs = [
+    {
+        id: 1,
+        date_opened: "14-Nov-2021",
+        job_number: "001/2021",
+        aircraft_reg: "5Y-BCZ",
+        aircraft_type: "RALLEY 235E",
+        job_status: "Opened",
+    },
+    {
+        id: 2,
+        date_opened: "23-Nov-2021",
+        job_number: "002/2021",
+        aircraft_reg: "5Y-BWE",
+        aircraft_type: "CESSNA 172L",
+        job_status: "Closed",
+    },
+    {
+        id: 3,
+        date_opened: "24-Nov-2021",
+        job_number: "003/2021",
+        aircraft_reg: "5Y-BCC",
+        aircraft_type: "CESSNA 172L",
+        job_status: "Opened",
+    },
+];
--- src/pages/private/jobs/tables/jobsList.jsx
+++ src/pages/private/jobs/tables/jobsList.jsx
@@ -1,25 +1,22 @@
-import { Fragment } from 'react';
+import { jobs } from "../../data/jobs";
 import { useNavigate } from "react-router-dom";
-import LockOpenIcon from '@mui/icons-material/LockOpen';
-import MoreEditDel from '../../../../components/shared/ActionMenu/MoreEditDel';
-import PrimarySearchBar from '../../../../components/shared/SearchBar/primary';
-import StyledTableCell from "../../../../components/shared/Table/StyledTableCell";
-import { Paper, Table, TableRow, TableHead, TableBody, TableCell, TableContainer } from "@mui/material";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import PrimaryTable from "../../../../components/shared/Table/primary";
+import MoreEditDel from "../../../../components/shared/ActionMenu/MoreEditDel";
+import PrimarySearchBar from "../../../../components/shared/SearchBar/primary";
 
-function createData(id, date_opened, job_number, aircraft_reg, aircraft_type, job_status) {
-    return {
-        id,
-        date_opened,
-        job_number,
-        aircraft_reg,
-        aircraft_type,
-        job_status,
-    };
-}
-
-const rows = [
-    createData(1, "23-Nov-2021", "002/2021", "5Y-BWE", "CESSNA 172L", "Opened"),
-    createData(2, "14-Nov-2021", "001/2021", "5Y-BCZ", "RALLYE 235E", "Opened"),
+const columns = [
+    { value: "date_opened", label: "Date Opened" },
+    { value: "job_number", label: "Job Number" },
+    { value: "aircraft_reg", label: "Aircraft Registration" },
+    { value: "aircraft_type", label: "Aircraft Type" },
+    { value: "job_status", label: "Job Status" },
+    {
+        value: "actions",
+        label: "Actions",
+        align: "center",
+        style: { padding: "1px" },
+    },
 ];
 
 export default function JobsList() {
@@ -30,39 +27,28 @@ export default function JobsList() {
     const handleView = (row) => navigate(`/jobs/job-profile/${row.id}`);
     const handleEdit = (row) => navigate(`/jobs/edit-job/${row.id}`);
 
+    const jobsData = jobs?.map((item) => {
+        return {
+            ...item,
+            actions: (
+                <MoreEditDel
+                    row={item}
+                    handleView={handleView}
+                    handleEdit={handleEdit}
+                />
+            ),
+        };
+    });
+
     return (
-        <Fragment>
-            <PrimarySearchBar action icon={<LockOpenIcon />} label="Open Job" navigate={openJob} />
-            <TableContainer component={Paper} elevation={0} sx={{ maxHeight: 500 }}>
-                <Table size="small" stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell align="left">Date Opened</StyledTableCell>
-                            <StyledTableCell align="left">Job Number</StyledTableCell>
-                            <StyledTableCell align="left">Aircraft Registration</StyledTableCell>
-                            <StyledTableCell align="left">Aircraft Type</StyledTableCell>
-                            <StyledTableCell align="left">Job Status</StyledTableCell>
-                            <StyledTableCell align="center">Actions</StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow hover key={row.id}>
-                                <TableCell align="left" scope="row"> {row.date_opened} </TableCell>
-                                <TableCell align="left">{row.job_number}</TableCell>
-                                <TableCell align="left">{row.aircraft_reg}</TableCell>
-                                <TableCell align="left">{row.aircraft_type}</TableCell>
-                                <TableCell align="left" style={{ color: row.job_status === "Opened" ? '#4caf50' : '#dc004e' }}>
-                                    {row.job_status}
-                                </TableCell>
-                                <TableCell align="center" style={{ padding: '0px' }}>
-                                    {<MoreEditDel row={row} handleView={handleView} handleEdit={handleEdit} />}
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Fragment>
+        <>
+            <PrimarySearchBar
+                action
+                icon={<LockOpenIcon />}
+                label="Open Job"
+                navigate={openJob}
+            />
+            <PrimaryTable data={jobsData} columns={columns} />
+        </>
     );
 }
