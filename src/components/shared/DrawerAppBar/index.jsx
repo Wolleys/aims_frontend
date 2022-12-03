0b558ccbd8ff474a5cb5eac8b91b5c7f2415fe2c
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-03 22:45:31 +0300

    Update: Add theme to desktop main menu

--- src/components/shared/DrawerAppBar/index.jsx
+++ src/components/shared/DrawerAppBar/index.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
-import AccountMenu from './AccountMenu';
-import MenuDrawer from './components/drawer';
-import MobileLogo from './components/mobile_logo';
-import DesktopLogo from './components/desktop_logo';
-import DesktopNavMenu from './components/desktop_nav_menu';
+import AccountMenu from "./AccountMenu";
+import MenuDrawer from "./components/drawer";
+import { useState, useContext } from "react";
+import MobileLogo from "./components/mobile_logo";
+import DesktopLogo from "./components/desktop_logo";
 import { AppBar, Box, Toolbar } from "@mui/material";
-
+import DesktopNavMenu from "./components/desktop_nav_menu";
+import { ThemeContext } from "../../../context/ThemeContextProvider";
 
 function DrawerAppBar(props) {
     const { window } = props;
+    const { theme } = useContext(ThemeContext);
     const [mobileOpen, setMobileOpen] = useState(false);
     const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
+    const navStyle = {
+        backgroundColor: theme === "light" ? "#3e345d" : "#161B22",
+        borderBottom: theme === "light" ? "none" : "1px solid #30363d"
+    }
+
     return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBar component="nav" style={{ background: '#3e345d' }} elevation={0}>
+        <Box sx={{ display: "flex" }}>
+            <AppBar component="nav" className="app_bar" sx={navStyle} elevation={0}>
                 <Toolbar>
                     <DesktopLogo handleDrawerToggle={handleDrawerToggle} />
                     <MobileLogo mobileOpen={mobileOpen} />
