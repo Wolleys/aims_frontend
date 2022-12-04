2b14ad250a4f92f97f3781b4f33498485e779e0a
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-04 22:34:31 +0300

    Update: Dark theme to search text field

--- src/components/shared/FormsUI/Search/StyledSearchTextField.jsx
+++ src/components/shared/FormsUI/Search/StyledSearchTextField.jsx
@@ -1,41 +1,45 @@
+import { useContext } from "react";
 import { TextField, styled } from "@mui/material";
-
-const SearchTextField = styled(TextField)(() => ({
-    "& .MuiOutlinedInput-root": {
-        "& input": {
-            color: "#666",
-            height: "unset",
-            fontWeight: 400,
-            fontSize: "14px",
-            padding: "6px 0 6px 12px",
-            "&::placeholder": {
-                color: "#03060b",
-                fontWeight: 500,
-            },
-        },
-        "& fieldset": {
-            borderRadius: "4px",
-            border: "1px solid #d5d8e1",
-        },
-        "&:hover fieldset": {
-            border: "1px solid #d5d8e1",
-        },
-        "&.Mui-focused fieldset": {
-            border: "1px solid #c6beee",
-        },
-        "& .MuiSvgIcon-root": {
-            color: "#ccc",
-            fontSize: "21px",
-        },
-    },
-}));
-
-const defaultProps = {
-    size: "small",
-    variant: "outlined",
-    placeholder: "Search...",
-};
+import { ThemeContext } from "../../../../context/ThemeContextProvider";
 
 export default function StyledSearchTextField({ ...props }) {
+    const { theme } = useContext(ThemeContext);
+
+    const SearchTextField = styled(TextField)(() => ({
+        "& .MuiOutlinedInput-root": {
+            "& input": {
+                height: "unset",
+                fontWeight: 400,
+                fontSize: "14px",
+                padding: "6px 0 6px 12px",
+                color: theme === "light" ? "#666" : "#c9d1d9",
+                "&::placeholder": {
+                    color: theme === "light" ? "#03060b" : "#c9d1d9",
+                    fontWeight: 500,
+                },
+            },
+            "& fieldset": {
+                borderRadius: "4px",
+                border: theme === "light" ? "1px solid #d5d8e1" : "1px solid #30363d",
+            },
+            "&:hover fieldset": {
+                border: theme === "light" ? "1px solid #d5d8e1" : "1px solid #30363d",
+            },
+            "&.Mui-focused fieldset": {
+                border: theme === "light" ? "1px solid #c6beee" : "1px solid #30363d",
+            },
+            "& .MuiSvgIcon-root": {
+                fontSize: "21px",
+                color: theme === "light" ? "#ccc" : "#8b949e",
+            },
+        },
+    }));
+
+    const defaultProps = {
+        size: "small",
+        variant: "outlined",
+        placeholder: "Search...",
+    };
+
     return <SearchTextField {...defaultProps} {...props} />;
 }
