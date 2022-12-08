71ca4cc42547f2cd8db3e0bc5cfe8002e6775707
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-08 23:18:45 +0300

    Update: Job profile theme colors

--- src/pages/private/jobs/actions/job_profile/jobCard.jsx
+++ src/pages/private/jobs/actions/job_profile/jobCard.jsx
@@ -1,8 +1,12 @@
 import dayjs from "dayjs";
+import { useContext } from "react";
+import { ThemeContext } from "../../../../../context/ThemeContextProvider";
 import { Box, Card, CardContent, CardHeader, Divider, Typography, Button, Grid } from "@mui/material";
 
 function JobCard(props) {
     const { item } = props;
+    const { theme } = useContext(ThemeContext);
+
     const jobStatus = item.job_status;
     const dateFrom = dayjs(item.date_opened);
     const dateTill = dayjs(item.date_closed);
@@ -28,30 +32,64 @@ function JobCard(props) {
         return jobData;
     }
 
+    const cardStyle = {
+        borderRadius: "6px",
+        backgroundColor: theme === "light" ? "#FAFAFA" : "#0d1117",
+        border: theme === "light" ? "1px solid #d0d7de" : "1px solid #30363d",
+    };
+
+    const defaultProps = {
+        sx: cardStyle,
+        variant: "outlined",
+    }
+
+    const titleTypoProps = {
+        fontSize: "16px",
+        fontWeight: "600",
+        color: theme === "light" ? "#24292f" : "#c9d1d9",
+    }
+
+    const nameTypoStyle = {
+        fontWeight: 600,
+        fontSize: "12px",
+        color: theme === "light" ? "#57606a" : "#8b949e",
+    }
+
+    const detailsTypoStyle = (item) => ({
+        float: "left",
+        fontWeight: 400,
+        fontSize: "12px",
+        color: item.detail === "Opened" ? "#4caf50"
+            : item.detail === "Closed" ? "#dc004e"
+            : theme === "light" ? "#24292f" : "#c9d1d9",
+    })
+
+    const snagViewStyle = {
+        fontSize: "12px",
+        textTransform: "none", 
+        color: theme === "light" ? "#0969da" : "#58a6ff", 
+
+    }
+
     return (
         <>
-            <Card sx={{ borderRadius: "6px" }} variant="outlined" >
-                <CardHeader title="Job Card"
-                    titleTypographyProps={{ fontSize: "16px", fontWeight: "600" }} />
+            <Card {...defaultProps} >
+                <CardHeader title="Job Card" titleTypographyProps={titleTypoProps} />
                 <CardContent sx={{ mt: "-10px", height: "400px" }}>
                     {jobDetails().map((item) => (
                         <Box key={item.name} >
                             <Grid container spacing={0}>
                                 <Grid item xs={6} md={6}>
-                                    <Typography style={{ color: "#03060b", fontWeight: 500, fontSize: "13px", }}>
+                                    <Typography sx={nameTypoStyle}>
                                         {item.name}
                                     </Typography>
                                 </Grid>
                                 <Grid item xs={6} md={6}>
-                                    <Typography style={{
-                                        color: item.detail === "Opened" ? "#4caf50"
-                                            : item.detail === "Closed" ? "#dc004e" : "#506176",
-                                        fontWeight: 400, fontSize: "13px", float: "left",
-                                    }}>{item.detail} </Typography>
+                                    <Typography sx={detailsTypoStyle(item)}>{item.detail} </Typography>
                                     <Box style={{ float: "right", margin: "-5px 0px -5px 0" }}>
                                         {item.name === "Snags recorded" && item.detail > 0 ?
-                                            <Button size="small"
-                                                sx={{ textTransform: "none", color: "#5046e4", fontSize: "13px" }}> view
+                                            <Button size="small" sx={snagViewStyle}> 
+                                                view
                                             </Button> : null
                                         }
                                     </Box>
