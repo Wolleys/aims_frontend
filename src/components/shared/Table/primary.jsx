390182d931cc3c64ab4c6706e674d497a132c777
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-09 17:37:57 +0300

    Update: Fallback error messages

--- src/components/shared/Table/primary.jsx
+++ src/components/shared/Table/primary.jsx
@@ -11,22 +11,35 @@ export default function PrimaryTable(props) {
             <Table size="small" stickyHeader>
                 <TableHead>
                     <TableRow>
-                        {columns?.map((head) => (
-                            <StyledTableCell align={head.align} key={head.value}> {head.label} </StyledTableCell>
-                        ))}
+                        {columns?.length > 0 ? (
+                            columns?.map((head) => (
+                                <StyledTableCell align={head.align} key={head.value}> {head.label} </StyledTableCell>
+                            ))
+                        ) : (
+                            <StyledTableCell colSpan="100%" sx={{ textAlign: "center" }}>
+                                <p>No table heads found!</p>
+                            </StyledTableCell>
+                        )}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {data?.map((row) => (
-                        <StyledTableRow key={row.id}>
-                            {columns?.map((col, index) => (
-                                <StyledTableCell key={index} align={col.align} style={col.style} > {row[col.value]} </StyledTableCell>
-                            ))}
+                    {data?.length > 0 ? (
+                        data?.map((row) => (
+                            <StyledTableRow key={row.id}>
+                                {columns?.map((col, index) => (
+                                    <StyledTableCell key={index} align={col.align} style={col.style} > {row[col.value]} </StyledTableCell>
+                                ))}
+                            </StyledTableRow>
+                        ))
+                    ) : (
+                        <StyledTableRow>
+                            <StyledTableCell colSpan="100%" sx={{ textAlign: "center", py: 6 }}>
+                                <p>No data found</p>
+                            </StyledTableCell>
                         </StyledTableRow>
-                    ))}
+                    )}
                 </TableBody>
             </Table>
-            {data ? null : <p>No data found</p>}
         </StyledTableContainer>
     );
 }
