0482aec52b64604d8e856542ee9b419d8b843c99
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-10 17:00:34 +0300

    Update: Delete dialog component theme colors

--- src/components/shared/DeleteDialog/index.jsx
+++ src/components/shared/DeleteDialog/index.jsx
@@ -1,4 +1,6 @@
-import { Fragment, forwardRef } from "react";
+import ErrorBtn from "../FormsUI/Button/ErrorBtn";
+import { Fragment, forwardRef, useContext } from "react";
+import { ThemeContext } from "../../../context/ThemeContextProvider";
 import { Dialog, DialogActions, Button, DialogContent, DialogContentText, DialogTitle, Slide } from "@mui/material";
 
 const Transition = forwardRef(function Transition(props, ref) {
@@ -6,26 +8,46 @@ const Transition = forwardRef(function Transition(props, ref) {
 });
 
 export default function DeleteDialog(props) {
+    const { theme } = useContext(ThemeContext);
     const { open, handleCloseDialog, row, handleDelete, handleClick } = props;
+
+    const paperPropsStyle = {
+        sx: {
+            boxShadow: "none",
+            backgroundColor: theme === "light" ? "#FAFAFA" : "#0d1117",
+            border: theme === "light" ? "1px solid #ff818266" : "1px solid #f8514966",
+        },
+    };
+
+    const dialogTitleStyle = {
+        paddingTop: 0.5,
+        marginBottom: 1,
+        fontSize: "14px",
+        fontWeight: "600",
+        paddingBottom: 0.5,
+        color: theme === "light" ? "#24292f" : "#c9d1d9",
+    }
+
+    const dialogContentTextStyle = {
+        fontSize: "14px",
+        color: theme === "light" ? "#24292f" : "#c9d1d9", 
+    }
 
     return (
         <Fragment>
-            <Dialog open={open} TransitionComponent={Transition} keepMounted onClose={handleCloseDialog} aria-describedby="alert" >
-                <DialogTitle sx={{ paddingBottom: 0.5, paddingTop: 0.5, fontSize: '16px', fontWeight: '600' }}>
-                    Confirm Delete
-                </DialogTitle>
-                <DialogContent dividers>
-                    <DialogContentText id="alert" sx={{ color: 'text.primary', fontSize: '15px' }} >
+            <Dialog open={open} TransitionComponent={Transition} keepMounted onClose={handleCloseDialog}
+                PaperProps={paperPropsStyle} >
+                <DialogTitle sx={dialogTitleStyle}> Are you absolutely sure? </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert" sx={dialogContentTextStyle} >
                         Once you delete, there is no going back. Please be certain.
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button variant="outlined" color="error" onClick={handleCloseDialog} 
-                        sx={{ textTransform: 'none', }} > Cancle
+                    <Button variant="outlined" color="error" onClick={handleCloseDialog}
+                        sx={{ textTransform: 'none', lineHeight: 1.3, fontSize: 14 }} > Cancle
                     </Button>
-                    <Button variant="contained" color="error" disableElevation sx={{ textTransform: 'none', }}
-                        onClick={() => { handleCloseDialog(); handleDelete(row.id); handleClick() }}> Delete
-                    </Button>
+                    <ErrorBtn onClick={() => { handleCloseDialog(); handleDelete(row.id); handleClick() }}> Delete </ErrorBtn>
                 </DialogActions>
             </Dialog>
         </Fragment>
