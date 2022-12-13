cafe0ecf5da33e2a79a6ef2ec563ce2ec9156db3
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-13 13:05:55 +0300

    Update: Loader component theme colors

--- src/components/shared/Loader/index.jsx
+++ src/components/shared/Loader/index.jsx
@@ -1,17 +1,21 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../../context/ThemeContextProvider";
 import { Box, CircularProgress, Backdrop, Typography, Stack } from "@mui/material";
 
 function Loader() {
+    const { theme } = useContext(ThemeContext);
+
     const backdropStyle = {
-        color: "#5046e4",
         position: "absolute",
-        backgroundColor: "#FAFAFA",
         zIndex: (theme) => theme.zIndex.drawer + 1,
+        color: theme === "light" ? "#5046e4" : "#8b949e",
+        backgroundColor: theme === "light" ? "#FAFAFA" : "#0d1117",
     }
     return (
         <Box >
             <Backdrop open sx={backdropStyle} >
                 <Stack gap={1} justifyContent="center" alignItems="center">
-                    <CircularProgress size={26} color="inherit" />
+                    <CircularProgress size={20} color="inherit" />
                     <Typography>Loading...</Typography>
                 </Stack>
             </Backdrop>
