aeb8ab0f0e7040e860b12a69785c184da934017d
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-14 05:47:46 +0300

    Add: Seperate directories for all profile pages

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 
 export const Profile = lazy(() => import("../../pages/private/profile"));
-export const Overview = lazy(() => import("../../pages/private/profile/Overview"));
-export const Details = lazy(() => import("../../pages/private/profile/Details"));
-export const Picture = lazy(() => import("../../pages/private/profile/Picture"));
-export const Security = lazy(() => import("../../pages/private/profile/Security"));
+export const Overview = lazy(() => import("../../pages/private/profile/overview"));
+export const Details = lazy(() => import("../../pages/private/profile/details"));
+export const Picture = lazy(() => import("../../pages/private/profile/picture"));
+export const Security = lazy(() => import("../../pages/private/profile/security"));
