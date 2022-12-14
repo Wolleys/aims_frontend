64c5b11310ebe8f405abc8cad9d9b51bc88ffcdf
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-14 06:32:42 +0300

    Update: Import routes using named aliases

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,17 +3,13 @@ import PrivateRoute from "./PrivateRoute";
 import { Routes, Route } from "react-router-dom";
 import Loader from "../components/shared/Loader";
 
-import { Login, ForgotPassword } from "./auth";
-import { Dashboard, Overview, Sales } from "./dash_board";
-import { Stock, DefaultStock } from "./stock/default";
-import {
-  PartEntry, DefaultPartEntry, AddPart, EditPart, PurchaseHistory, NewPurchase,
-  EditPurchase, IssueHistory, PurchasePart
-} from "./part_entry/default";
-
-import { Jobs, DefaultJobsList, OpenJob, EditJob, JobProfile } from "./jobs";
-import { PartIssue, DefaultPartIssue } from "./part_issue/default";
-import { HangarUse, DefaultHangarUse, IssuePart, EditIssuedPart } from "./hangar_use/default";
+import * as Auth from "./auth";
+import * as Dashboard from "./dash_board";
+import * as Stock from "./stock/default";
+import * as Part from "./part_entry/default";
+import * as Jobs from "./jobs";
+import * as PartIssue from "./part_issue/default";
+import * as HangarUse from "./hangar_use/default";
 import * as Profile from "./profile";
 import { NotFound } from "./not_found";
 
@@ -21,38 +17,38 @@ function AppRoutes() {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
-        <Route path="dashboard" element={<PrivateRoute> <Dashboard /> </PrivateRoute>}>
-          <Route path="overview" element={<Overview />} />
-          <Route path="sales" element={<Sales />} />
+        <Route path="/" element={<Auth.Login />} />
+        <Route path="forgot-password" element={<Auth.ForgotPassword />} />
+        <Route path="dashboard" element={<PrivateRoute> <Dashboard.Dashboard /> </PrivateRoute>}>
+          <Route path="overview" element={<Dashboard.Overview />} />
+          <Route path="sales" element={<Dashboard.Sales />} />
         </Route>
-        <Route path="stock" element={<PrivateRoute> <Stock /> </PrivateRoute>} >
-          <Route path="default" element={<DefaultStock />} />
+        <Route path="stock" element={<PrivateRoute> <Stock.Stock /> </PrivateRoute>} >
+          <Route path="default" element={<Stock.DefaultStock />} />
         </Route>
-        <Route path="part-entry" element={<PrivateRoute> <PartEntry /> </PrivateRoute>} >
-          <Route path="default" element={<DefaultPartEntry />} />
-          <Route path="default/add-part" element={<AddPart />} />
-          <Route path="default/edit-part/:partId" element={<EditPart />} />
-          <Route path="default/purchase-history/:partId" element={<PurchaseHistory />} />
-          <Route path="default/purchase-history/new-purchase" element={<NewPurchase />} />
-          <Route path="default/purchase-history/edit-part/:partId" element={<EditPurchase />} />
-          <Route path="default/issue-history/:partId" element={<IssueHistory />} />
-          <Route path="default/purchase-part/:partId" element={<PurchasePart />} />
+        <Route path="part-entry" element={<PrivateRoute> <Part.PartEntry /> </PrivateRoute>} >
+          <Route path="default" element={<Part.DefaultPartEntry />} />
+          <Route path="default/add-part" element={<Part.AddPart />} />
+          <Route path="default/edit-part/:partId" element={<Part.EditPart />} />
+          <Route path="default/purchase-history/:partId" element={<Part.PurchaseHistory />} />
+          <Route path="default/purchase-history/new-purchase" element={<Part.NewPurchase />} />
+          <Route path="default/purchase-history/edit-part/:partId" element={<Part.EditPurchase />} />
+          <Route path="default/issue-history/:partId" element={<Part.IssueHistory />} />
+          <Route path="default/purchase-part/:partId" element={<Part.PurchasePart />} />
         </Route>
-        <Route path="jobs" element={<PrivateRoute> <Jobs /> </PrivateRoute>} >
-          <Route path="jobs-list" element={<DefaultJobsList />} />
-          <Route path="open-job" element={<OpenJob />} />
-          <Route path="edit-job/:jobId" element={<EditJob />} />
-          <Route path="job-profile/:jobId" element={<JobProfile />} />
+        <Route path="jobs" element={<PrivateRoute> <Jobs.Jobs /> </PrivateRoute>} >
+          <Route path="jobs-list" element={<Jobs.DefaultJobsList />} />
+          <Route path="open-job" element={<Jobs.OpenJob />} />
+          <Route path="edit-job/:jobId" element={<Jobs.EditJob />} />
+          <Route path="job-profile/:jobId" element={<Jobs.JobProfile />} />
         </Route>
-        <Route path="part-issue" element={<PrivateRoute> <PartIssue /> </PrivateRoute>} >
-          <Route path="default" element={<DefaultPartIssue />} />
+        <Route path="part-issue" element={<PrivateRoute> <PartIssue.PartIssue /> </PrivateRoute>} >
+          <Route path="default" element={<PartIssue.DefaultPartIssue />} />
         </Route>
-        <Route path="hangar-use" element={<PrivateRoute> <HangarUse /> </PrivateRoute>} >
-          <Route path="default" element={<DefaultHangarUse />} />
-          <Route path="default/issue-part" element={<IssuePart />} />
-          <Route path="default/edit-issue/:partId" element={<EditIssuedPart />} />
+        <Route path="hangar-use" element={<PrivateRoute> <HangarUse.HangarUse /> </PrivateRoute>} >
+          <Route path="default" element={<HangarUse.DefaultHangarUse />} />
+          <Route path="default/issue-part" element={<HangarUse.IssuePart />} />
+          <Route path="default/edit-issue/:partId" element={<HangarUse.EditIssuedPart />} />
         </Route>
         <Route path="profile" element={<PrivateRoute> <Profile.Profile /> </PrivateRoute>}>
           <Route path="overview" element={<Profile.Overview />} />
