fb9ee52de3d66e9fc7d2dbbfd96194e3f7bf8645
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-15 07:45:22 +0300

    Update: Stock tabs and layout

--- src/components/tabs/stock/index.jsx
+++ src/components/tabs/stock/index.jsx
@@ -1,18 +1,16 @@
-import { NavLink } from 'react-router-dom';
-import { Box, Tabs, Tab, Container } from '@mui/material';
-import StorefrontIcon from '@mui/icons-material/Storefront';
+import Layout from "../Layout";
+import StyledTab from "../StyledTab";
+import StyledTabs from "../StyledTabs";
+import StorefrontIcon from "@mui/icons-material/Storefront";
 
 function StockTabs() {
     return (
-        <Container maxWidth="fixed">
-            <Box sx={{ width: '100%', }}>
-                <Tabs value={'Default'} centered
-                    TabIndicatorProps={{ sx: { display: 'none', top: "60px" } }}>
-                    <Tab value='Default' component={NavLink} to='/stock/default'
-                        label={<span><StorefrontIcon /><br /> {'Default'} </span>} sx={{ textTransform: 'none', }} wrapped />
-                </Tabs>
-            </Box>
-        </Container>
+        <Layout >
+            <StyledTabs value={"Default"} >
+                <StyledTab value="Default" to="/stock/default"
+                    label={<span><StorefrontIcon /><br /> {"Default"} </span>} />
+            </StyledTabs>
+        </Layout>
     )
 }
 
