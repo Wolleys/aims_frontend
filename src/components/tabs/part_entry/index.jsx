1e205250aaf6bbaebfc2d8ace43cc52fad92f8d2
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-15 07:49:22 +0300

    Update: Part entry tabs and layout

--- src/components/tabs/part_entry/index.jsx
+++ src/components/tabs/part_entry/index.jsx
@@ -1,18 +1,16 @@
-import { NavLink } from 'react-router-dom';
-import StoreIcon from '@mui/icons-material/Store';
-import { Box, Tabs, Tab, Container } from '@mui/material';
+import Layout from "../Layout";
+import StyledTab from "../StyledTab";
+import StyledTabs from "../StyledTabs";
+import StoreIcon from "@mui/icons-material/Store";
 
 function PartEntryTabs() {
     return (
-        <Container maxWidth="fixed">
-            <Box sx={{ width: '100%', }}>
-                <Tabs value={'Default'} centered
-                    TabIndicatorProps={{ sx: { display: 'none', top: "60px" } }}>
-                    <Tab value='Default' component={NavLink} to='/part-entry/default'
-                        label={<span><StoreIcon /><br /> {'Default'} </span>} sx={{ textTransform: 'none', }} wrapped />
-                </Tabs>
-            </Box>
-        </Container>
+        <Layout>
+            <StyledTabs value={"Default"} >
+                <StyledTab value="Default" to="/part-entry/default"
+                    label={<span><StoreIcon /><br /> {"Default"} </span>} />
+            </StyledTabs>
+        </Layout>
     )
 }
 
--- src/components/tabs/stock/index.jsx
+++ src/components/tabs/stock/index.jsx
@@ -5,7 +5,7 @@ import StorefrontIcon from "@mui/icons-material/Storefront";
 
 function StockTabs() {
     return (
-        <Layout >
+        <Layout>
             <StyledTabs value={"Default"} >
                 <StyledTab value="Default" to="/stock/default"
                     label={<span><StorefrontIcon /><br /> {"Default"} </span>} />
