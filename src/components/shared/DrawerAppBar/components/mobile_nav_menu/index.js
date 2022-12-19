f7c005d38513d71ed29b7fbf640fda700b766515
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-19 12:47:31 +0300

    Update: Highlight selected menu item icons

--- src/components/shared/DrawerAppBar/components/mobile_nav_menu/index.js
+++ src/components/shared/DrawerAppBar/components/mobile_nav_menu/index.js
@@ -1,15 +1,14 @@
-import { navItems } from "../nav_items";
 import { useContext } from "react";
+import { navItems } from "../nav_items";
 import { Link, MenuItem } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
-import { ThemeContext } from "../../../../../context/ThemeContextProvider";
-
+import JobsLinks from "../nav_items/default/jobsLinks";
 import DashboardLinks from "../nav_items/dashboardLinks";
 import StockLinks from "../nav_items/default/stockLinks";
 import PartEntryLinks from "../nav_items/default/partEntryLinks";
-import JobsLinks from "../nav_items/default/jobsLinks";
 import PartIssueLinks from "../nav_items/default/partIssueLinks";
 import HangarUseLinks from "../nav_items/default/hangarUseLinks";
+import { ThemeContext } from "../../../../../context/ThemeContextProvider";
 
 function MobileNavMenu() {
     const { theme } = useContext(ThemeContext);
@@ -21,7 +20,7 @@ function MobileNavMenu() {
     const menuItemStyle = {
         p: 2,
         fontSize: "14px",
-        color: theme === "light" ? "#24292f" : "#f0f6fc",
+        color: theme === "light" ? "#24292f" : "#c9d1d9",
         borderBottom: theme === "light" ? "thin solid #d0d7de" : "thin solid #30363d",
         "& .MuiSvgIcon-root": {
             fontSize: "18px",
@@ -36,6 +35,11 @@ function MobileNavMenu() {
         },
         "&.Mui-selected, &.Mui-selected:hover": {
             backgroundColor: theme === "light" ? "#F5F5F5" : "#161b22",
+        },
+        "&.Mui-selected": {
+            "& .MuiSvgIcon-root": {
+                color: theme === "light" ? "#5046e4" : "#8B949E",
+            },
         },
     }
 
