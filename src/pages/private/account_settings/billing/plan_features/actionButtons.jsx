5a3b18c0d86c142882b7b0807b112de852ca6156
Wolleys Migaya <wolleysmigaya@gmail.com>
2022-12-23 10:54:01 +0300

    Update: Add navigation links

--- src/pages/private/account_settings/billing/plan_features/actionButtons.jsx
+++ src/pages/private/account_settings/billing/plan_features/actionButtons.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import ActionBtn from "../../../../../components/shared/FormsUI/Button/ActionBtn";
 import UtilityBtn from "../../../../../components/shared/FormsUI/Button/UtilityBtn";
 
 function ActionButtons() {
+    const navigate = useNavigate();
+    const goTo = () => navigate("/settings/billing/plans");
+
     const actionBtnStyle = {
         mr: 1,
         ml: 1,
@@ -10,8 +14,8 @@ function ActionButtons() {
 
     return (
         <>
-            <UtilityBtn style={{ fontSize: "13px" }}>Compare all plans</UtilityBtn>
-            <ActionBtn sx={actionBtnStyle}>Upgrade</ActionBtn>
+            <UtilityBtn onClick={goTo} style={{ fontSize: "13px" }}>Compare all plans</UtilityBtn>
+            <ActionBtn onClick={goTo} sx={actionBtnStyle}>Upgrade</ActionBtn>
         </>
     );
 }
